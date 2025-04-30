67f2c629479f5ec57b6d1dc9472902c276f5e875
bhavanithyagaraju <bhavaniveena779@gmail.com>
2025-05-01 02:02:50 +0530

    express server created

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
-// Step 1: Import the http module
-const http = require('http');
+// import the expressjs step:1
+const express =require ('express');
 
-// Step 2: Create a server
-// post man inside this url is working or not checking
-const server = http.createServer((req, res) => {
-  // this is showing in your vs terminal
-  // this is used requst wich mothod is used in postman and which url is used
-  console.log("Request Received");
-  console.log(`Request URL: ${req.url}`);
-  console.log(`Request MOTHOD: ${req.method}`);
+const app=express();
 
-  // console.log(`Request URL:${res.url}`);
-  // this "res.end" this is showing in your postman terminal
-  res.end("request is send to postman"); 
-});
+// postmam stauts step:3
+app.get('/',(req,res)=>{
+  res.send('testing the server is working fine or not')
+})
 
-// Step 3: Start the server
-server.listen(3001, '127.0.0.1', () => {
-  console.log("Server is running at http://127.0.0.1:3001");
-});
+// start the server step:2
+app.listen(3001,'127.0.0.1',()=>{
+  console.log("The server is running at http://127.0.0.1:3001");
+})
