92a8d6ebf66a6cab34b01971c1823e1c71a3560a
bhavanithyagaraju <bhavaniveena779@gmail.com>
2025-05-01 21:45:52 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const app = require ('./app')
+const app = require ('./app');
+const mogoose =require('mongoose');
+require('dotenv').config();
+
+
+mogoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+console.log("successfully mongodb connected");
+
+}).catch((err)=>{
+console.log(`mongodb connection failed ${err.message}`);
+
+})
 
 app.listen(3001,'127.0.0.1',()=>{
 console.log("The server is running http//127.0.0.1:3001");
 })
+
+
 
